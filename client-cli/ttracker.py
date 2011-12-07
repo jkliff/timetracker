34e370a05aec66b19cf2c90c63d02f978c3f5c1a
john <john@hesse>
2011-12-07 23:40:27 +0100

    preliminary

---
+++ client-cli/ttracker.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+
+def _execute ():
+    """here we execute the operation. we build here the connection as to delay it as most as possible"""
+    #validate parameters
+
+    #open connection
+    #send request
+    result = ()
+    #close
+    return result
+
+def insert ():
+    #build request for current date
+    #send request
+    #check it is all ok
+
+def finish_current ():
+    pass
+
+def view ():
+    pass
+
+def check ():
+    """send a probe to the server. useful to check if it is reacheble and rightly configured"""
+    pass
+
+def main ():
+    #checks parameters
+
+    #checks we have configuration avaliable at the right place (overriden or default)
+
+if __name__ == '__main__':
+    main ()
