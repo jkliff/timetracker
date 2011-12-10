842af4642aba4b550250b8d95a2a33d97b8da6eb
john <john@hesse>
2011-12-10 23:36:11 +0100

    (unfinished) actually make rest calls; added configuration

--- client-cli/ttracker.py
+++ client-cli/ttracker.py
@@ -1,71 +1,69 @@
-#!/usr/bin/python
+#!/usr/local/bin/python2.7
 
+import yaml
+import os.path
 from argparse import ArgumentParser
+import urllib2
+import re
 
-def generate_action (action):
+sanitize_url = lambda x: 'http://' + re.sub ('\/+', '/', re.sub ('(http://)(.*)', '\g<2>', x))
+
+def generate_action (action_path, http_method):
     """generates a function capable of submitting rest requests to the server"""
-    pass
+    def f (config, msg):
+        r = ()
+        host = config ['server_base_url']
+        v = '%s/%s' % (host, action_path) 
+        v = sanitize_url (v)
+        req = urllib2.urlopen (v)
+
+        return r
+    return f
 
 ACTIONS = {
-    'add': generate_action ('add'),
-    'stop': generate_action ('stop'),
-    'list': generate_action ('list'),
-    'status': generate_action ('status')
+    'add': generate_action ('/activity/add', 'PUT'),
+    'stop': generate_action ('/activity/stop', ''),
+    'list': generate_action ('/activity/list', 'GET'),
+    'status': generate_action ('/status', 'GET')
 }
 
-def _execute ():
-    """here we execute the operation. we build here the connection as to delay it as most as possible"""
-    #validate parameters
-
-    #open connection
-    #send request
-    result = ()
-    #close
-    return result
-
-def insert ():
-    #build request for current date
-    #send request
-    #check it is all ok
-
-    pass
-
-def finish_current ():
-    pass
-
-def view ():
-    pass
-
-def check ():
-    """send a probe to the server. useful to check if it is reacheble and rightly configured"""
-    pass
-
-def report_wrong_action ():
-    print 'Unsupported operation'
-
-def setup_argument_parser ():
+def _setup_argument_parser ():
 
     p = ArgumentParser (description = """timetracker command-line client.""")
     p.add_argument ('-a', '--action', choices = ACTIONS.keys(), dest='action', help='Action to be executed')
     p.add_argument ('-m', '--message', dest = 'message', help='message to add', metavar =  'MSG')
+    p.add_argument ('-c', '--config', dest = 'config', default = '~/.timetracker.conf', help='configuration', metavar = 'CONFIG_PATH')
     return p
 
+_npath = lambda p: os.path.abspath (os.path.expanduser (p))
+
 def main ():
-    p = setup_argument_parser ()
+    p = _setup_argument_parser ()
     args = p.parse_args()
-    print args
+
     action = args.action
     message = args.message
-    
-    print action, message
-    
+    config_path = _npath (args.config)
+
     if action is None:
         print ("""I can't read your mind.""")
         exit (-1)
-    
-    print 'Will do', action
 
     #checks we have configuration avaliable at the right place (overriden or default)
+    if config_path is None or not os.path.exists (config_path):
+        print ('Configuration could not be found at %s' % config_path)
+        exit (-2)
+    else:
+        # try to load yaml from specified path
+        config = {}
+        with (open (config_path, 'r')) as f:
+            config = yaml.load (f.read ())
+
+        print config 
+
+    # performs action
+    global ACTIONS
+    ACTIONS [action] (config, message)
 
 if __name__ == '__main__':
     main ()
